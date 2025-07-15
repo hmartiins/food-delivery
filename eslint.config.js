22960f02b15a4f0d53e5d73d2032f601b93fc506
Henrique Martins <hmartins.dev@gmail.com>
2025-07-15 08:26:40 -0300

    feat: add prettier and eslint in the project

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,30 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettier = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', '.expo/*', 'node_modules/*'],
+  },
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      // Desabilitar regras que conflitam com Prettier
+      'arrow-parens': 'off',
+      'object-curly-spacing': 'off',
+      quotes: 'off',
+      semi: 'off',
+      'space-before-function-paren': 'off',
+      'comma-dangle': 'off',
+      indent: 'off',
+      'max-len': 'off',
+      'no-trailing-spaces': 'off',
+      'eol-last': 'off',
+    },
   },
 ]);
