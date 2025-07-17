e7d53f782ed86028f35ef8229a929c659962a894
Henrique Martins <hmartins.dev@gmail.com>
2025-07-16 23:45:59 -0300

    feat: create CustomButton

---
+++ components/CustomButton.tsx
@@ -0,0 +1,29 @@
+import { CustomButtonProps } from '@/type';
+import cn from 'clsx';
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native';
+
+const CustomButton = ({
+  onPress,
+  title,
+  style,
+  leftIcon,
+  textStyle,
+  isLoading,
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity onPress={onPress} className={cn('custom-btn', style)}>
+      {leftIcon}
+      <View className="flex-center flex-row">
+        {isLoading ? (
+          <ActivityIndicator size="small" color="#fff" />
+        ) : (
+          <Text className={cn('paragraph-semibold text-white-100', textStyle)}>
+            {title}
+          </Text>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
