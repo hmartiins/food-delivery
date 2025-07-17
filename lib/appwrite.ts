97ea2f52d77273bec3d0b234b7c13e95739aebbc
Henrique Martins <hmartins.dev@gmail.com>
2025-07-16 23:45:21 -0300

    feat: add appwrite configuration

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appWriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
+  platform: 'com.hmartiins.fooddelivery',
+};
