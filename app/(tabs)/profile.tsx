dc43afb8710ee620333d35cd23f0884813d5ece9
Henrique Martins <hmartins.dev@gmail.com>
2025-09-14 16:10:43 -0300

    feat: revert profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,26 +1,5 @@
-import { useState } from 'react';
-
-import { Text, TouchableOpacity, View } from 'react-native';
-
-import { BottomSheet } from '@/components';
+import { View } from 'react-native';
 
 export default function Profile() {
-  const [bottomSheetVisible, setBottomSheetVisible] = useState(true);
-
-  function handleCloseBottomSheet() {
-    setBottomSheetVisible(prev => !prev);
-  }
-
-  return (
-    <View className="h-full bg-white">
-      <TouchableOpacity
-        className="mt-20"
-        onPress={() => setBottomSheetVisible(true)}
-      >
-        <Text>Open Bottom Sheet</Text>
-      </TouchableOpacity>
-
-      {bottomSheetVisible && <BottomSheet onClose={handleCloseBottomSheet} />}
-    </View>
-  );
+  return <View className="h-full bg-white"></View>;
 }
